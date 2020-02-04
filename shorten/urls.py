a556463635aab37c787dc44af1f527f7fc1ffc9e
bshukla3041 <bhavesh.shuklatz@gmail.com>
2020-02-04 23:15:18 +0530

    Update to v2

--- shorten/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from .views import index_view, url_redirect_view
-
-urlpatterns = [
-    path('', index_view),
-    re_path(r'^(?P<shortcode>[\w-]+)/$', url_redirect_view, name='shortcode'),
-]
